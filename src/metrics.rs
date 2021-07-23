97ae632b370dada1447db01036be236fc01378da
jrconlin <jr+git@mozilla.com>
2021-07-23 15:40:27 -0700

    f bump up cadence

--- src/metrics.rs
+++ src/metrics.rs
@@ -5,7 +5,7 @@ use std::time::Instant;
 
 use actix_web::{error::ErrorInternalServerError, web::Data, Error, HttpRequest};
 use cadence::{
-    BufferedUdpMetricSink, Counted, Metric, NopMetricSink, QueuingMetricSink, StatsdClient, Timed,
+    BufferedUdpMetricSink, Counted, CountedExt, Metric, NopMetricSink, QueuingMetricSink, StatsdClient, Timed,
 };
 
 use crate::error::{HandlerError, HandlerErrorKind};
