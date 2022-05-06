7e1f090c655af9beba658bd9bfee21e83a2769f1
Raphael Pierzina <raphael@hackebrot.de>
2022-05-07 00:37:55 +0200

    fix: Add get_id_token function and Authorization header
    Client cloud functions are configured to require a valid ID token
    associated with a service account that has the invoke permission.
    This change adds a method to request an ID token and adds it as
    a header to the request invoking the client functions.
    
    Co-authored-by: Dustin Lactin <dlactin@mozilla.com>

--- smoke-tests/runner/main.py
+++ smoke-tests/runner/main.py
@@ -7,6 +7,8 @@ from dataclasses import asdict, dataclass, field
 from enum import Enum
 from typing import Any, Dict, List
 
+import google.auth.transport.requests
+import google.oauth2.id_token
 import requests
 from flask import Request, Response, abort, jsonify
 
@@ -60,6 +62,15 @@ class ResponseData:
     results: Dict = field(default_factory=dict)
 
 
+def get_id_token(audience):
+    """Fetch an oauth2 ID token for triggering other functions."""
+
+    auth_req = google.auth.transport.requests.Request()
+    id_token = google.oauth2.id_token.fetch_id_token(auth_req, audience)
+
+    return id_token
+
+
 def run_geo_smoke_tests(request: Request):
     """Triggered by HTTP Cloud Function."""
 
@@ -87,6 +98,7 @@ def run_geo_smoke_tests(request: Request):
         response_data.results[env.name] = {}
         for client in Clients:
             url = os.environ[f"CLIENT_URL_{client.name}"]
+            id_token = get_id_token(url)
             response = requests.post(
                 url,
                 json={
@@ -94,6 +106,7 @@ def run_geo_smoke_tests(request: Request):
                     "expected_country": client.value.country,
                     "expected_region": client.value.region,
                 },
+                headers={"Authorization": f"Bearer {id_token}"},
             )
             response_data.results[env.name][client.name] = ClientResponse(
                 status_code=response.status_code, content=response.text
