b4dfb26dcb3ce76ccbd092203905e46c5207cc40
Katrina Anderson <3422688+Trinaa@users.noreply.github.com>
2023-04-25 21:05:13 -0400

    test: add load test solution

---
+++ test-engineering/load/common/user_agent.py
@@ -0,0 +1,46 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+"""User-Agent client data module."""
+
+FIREFOX_USER_AGENTS: list[str] = [
+    "Mozilla/5.0 (Windows NT 10.0; rv:10.0) Gecko/20100101 Firefox/91.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:10.0) Gecko/20100101 "
+    "Firefox/91.0",
+    "Mozilla/5.0 (X11; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0",
+    "Mozilla/5.0 (Android 11; Mobile; rv:92.0) Gecko/92.0 Firefox/92.0",
+    "Mozilla/5.0 (Android; Tablet; rv:92.0) Gecko/92.0 Firefox/92.0",
+    "Mozilla/5.0 (iPod touch; CPU iPhone OS 8_3 like Mac OS X) "
+    "AppleWebKit/600.1.4 (KHTML, like Gecko) FxiOS/1.0 Mobile/12F69 "
+    "Safari/600.1.4",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 11_5_1 like Mac OS X) "
+    "AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/35.0 Mobile/15E148 "
+    "Safari/605.1.15",
+    "Mozilla/5.0 (iPad; CPU iPhone OS 8_3 like Mac OS X) "
+    "AppleWebKit/600.1.4 (KHTML, like Gecko) FxiOS/1.0 "
+    "Mobile/12F69 Safari/600.1.4",
+]
+
+NON_FIREFOX_USER_AGENTS: list[str] = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, "
+    "like Gecko) Chrome/58.0.3029.110 SafariSafari/537.36",
+    "Mozilla/5.0 (Linux; Android 6.0.1; SAMSUNG SM-G570Y Build/MMB29K) "
+    "AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/4.0 "
+    "Chrome/44.0.2403.133 Mobile Safari/537.36",
+    "Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) "
+    "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile "
+    "Safari/537.36",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) "
+    "AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/69.0.3497.105 "
+    "Mobile/15E148 Safari/605.1",
+    "Mozilla/5.0 (Linux; Android 7.0; Pixel C Build/NRD90M; wv) "
+    "AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/52.0.2743.98 "
+    "Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, "
+    "like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
+    "Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like "
+    "Gecko) Chrome/51.0.2704.64 Safari/537.36",
+    "Opera/9.80 (Android; Opera Mini/6.5.27452/26.1305; U; ja) Presto/2.8.119 "
+    "Version/10.54",
+]
